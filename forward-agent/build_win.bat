e3bd66b9090e759e30ba83f7469762704f1b305f
tavenli <tavenli@qq.com>
2020-11-04 10:07:53 +0800

    -

--- forward-agent/build_win.bat
+++ forward-agent/build_win.bat
@@ -1,10 +1,9 @@
 
-set GoDevWork="D:\CodeWork\port-forward-v2\"
 
 echo "Build For windows..."
 set GOOS=windows
 set GOARCH=amd64
-set GOPATH=%GoDevWork%;%GOPATH%
+
 go build -o forward-agent.exe
 
 echo "--------- Build For windows Success!"
